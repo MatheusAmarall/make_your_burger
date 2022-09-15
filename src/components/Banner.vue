045c299cd39f8bd0c281446cfe1fe66b460c2168
matheus <m.amaral8998@gmail.com>
2022-09-15 17:13:10 -0300

    make_your_burger

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main-banner">
+        <h1>Make Your Burger</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Banner"
+}
+</script>
+
+<style scoped>
+    #main-banner {
+        background-image: url('/public/img/burger.jpg');
+        height: 500px;
+        background-size: cover;
+        background-position: 0 -250px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1 {
+        color: #fff;
+        background-color: #222;
+        padding: 20px 40px;
+        text-align: center;
+        font-size: 60px;
+    }
+</style>
